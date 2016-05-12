e019e61f2ef7ee8d0cf238413961343b9104278f
David <davidjoshuasim@gmail.com>
2016-05-12 13:13:09 -0700

    updated small typo

--- files/garage_gpio.py
+++ files/garage_gpio.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 #import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(19, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
 GPIO.output(18, GPIO.HIGH)
-#time.sleep(0.25) 
+#time.sleep(0.25)
 GPIO.output(18, GPIO.LOW)
