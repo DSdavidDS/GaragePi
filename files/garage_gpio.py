f4d8d31a38ebc6a37f651abc5be3f75d239dc625
David <davidjoshuasim@gmail.com>
2016-05-16 10:39:23 -0700

    updated script

--- files/garage_gpio.py
+++ files/garage_gpio.py
@@ -1,9 +1,8 @@
 import RPi.GPIO as GPIO
-#import time
+import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
-GPIO.output(18, GPIO.HIGH)
-#time.sleep(0.25)
-GPIO.output(18, GPIO.LOW)
+GPIO.output(18, GPIO.LOW) # reverse logic
+time.sleep(.2)
 GPIO.cleanup()
